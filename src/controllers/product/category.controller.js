d8a59c9e19cdd0889aa7db5f9f908512304b0dac
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-09-05 15:22:49 -0600

    Se editó controlador de categoría

--- src/controllers/product/category.controller.js
+++ src/controllers/product/category.controller.js
@@ -5,11 +5,11 @@ export default class ProductCategoryController {
 
     static async getRecentCategories(req, res) {
         try {
-            const recentRecipes = await ProductCategoryModel.find({}).sort({ date: 'descending'});
+            const recentRecipes = await ProductCategoryModel.find({}).sort({ date: 'descending'}).limit(6);
             res.status(200).send({ success: true, message: 'Categorías recientes', data: recentRecipes });
 
         } catch (error) {
-            res.status(500).send({ success: false, message: 'Error al obtener categorías' });
+            res.status(500).send({ success: false, message: 'Error al obtener categorías', data: [] });
         }
     }
 
