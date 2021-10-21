ec41584703795b8834e12e252efe30035ce331e6
Angel Herrarte <angelherrarte3@gmail.com>
2021-10-21 17:10:52 -0600

    Se editó lógica para eliminar item de carrito

--- src/utils/shoppingCart.js
+++ src/utils/shoppingCart.js
@@ -2,7 +2,7 @@ export function addProductToShoppingCart(product, quantity, shoppingCart) {
     const productIndex = shoppingCart.products.findIndex((item) => item._id.toString() === product._id.toString());
     if (productIndex === -1) shoppingCart.products.push({ _id: product._id, quantity: quantity });
     if (productIndex !== -1) shoppingCart.products[productIndex].quantity += quantity;
-    
+
     shoppingCart.total += (product.price * quantity);
     shoppingCart.subTotal = shoppingCart.total;
     
@@ -13,11 +13,16 @@ export function deleteProductFromShoppingCart(product, quantity, shoppingCart) {
     const productIndex = shoppingCart.products.findIndex((item) => item._id.toString() === product._id.toString());
     
     const previusQuantity = shoppingCart.products[productIndex].quantity;
-    if (quantity >= previusQuantity) shoppingCart.products.splice(productIndex, 1);
-    else shoppingCart.products[productIndex].quantity -= quantity;
-    
-    shoppingCart.total -= (product.price * previusQuantity);
-    shoppingCart.subTotal = shoppingCart.total;
-    
+    if (quantity >= previusQuantity) {
+        shoppingCart.products.splice(productIndex, 1);
+        shoppingCart.total = 0;
+        shoppingCart.subTotal = shoppingCart.total;
+
+    } else {
+        shoppingCart.products[productIndex].quantity -= quantity;
+        shoppingCart.total -= (product.price * quantity);
+        shoppingCart.subTotal = shoppingCart.total;
+    }
+
     return shoppingCart;
 }
