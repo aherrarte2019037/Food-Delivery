67d7dc9ef0c140b24d9cc92dbd959a9a811e39fb
Angel Herrarte <angelherrarte3@gmail.com>
2021-11-23 14:00:56 -0600

    Controlador de orden editado

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -27,7 +27,14 @@ export default class OrderController {
                 };
             }
 
-            let orders = await OrderModel.find(isDelivery ? { status: { $ne: 'PAGADO' } } : {}).populate('delivery address').populate('user', '-password').populate({ path: 'cart', populate: { path: 'products._id', populate: { path: 'category' } } });            
+            let deliveryQuery = {
+                $and: [
+                    { status: { $ne: 'PAGADO' } },
+                    { delivery: user._id }
+                ]
+            };
+
+            let orders = await OrderModel.find(isDelivery ? deliveryQuery : {}).populate('delivery address').populate('user', '-password').populate({ path: 'cart', populate: { path: 'products._id', populate: { path: 'category' } } });            
             orders = orders.map(element => {
                 let order = element.toObject();  
                 const products = order.cart.products.map(product => new Object({ quantity: product.quantity, product: product._id } ));
@@ -35,7 +42,7 @@ export default class OrderController {
 
                 return order;
             });
-            
+                        
             const grouped = groupBy(orders, 'status');
             res.status(200).send({ success: true, message: 'Pedidos agrupados', data: grouped });
             
@@ -106,8 +113,35 @@ export default class OrderController {
             res.status(200).send({ success: true, message: 'Repartidor asignado', data: response });
 
         } catch (error) {
-            console.log(error)
             res.status(500).send({ success: false, message: 'Error al asignar repartidor', error });
+        }
+    }
+
+    static async edit(req, res) {
+        try {
+            const order = req.params.id;
+            const updateData = req.body;
+            delete updateData.user;
+
+            const orderFound = await OrderModel.findById(order);
+            if (!orderFound ) return res.status(500).send({ success: false, message: 'Pedido no encontrado' });
+
+            if (updateData.status) updateData.status = updateData.status.toUpperCase();
+
+            const orderEdited = await OrderModel.findByIdAndUpdate(order, updateData);
+            if (!orderEdited ) return res.status(500).send({ success: false, message: 'Error al editar pedido' });
+
+            await orderEdited.populate('delivery address').populate('user', '-password').populate({ path: 'cart', populate: { path: 'products._id', populate: { path: 'category' } } }).execPopulate();
+
+            let response = orderEdited.toObject();  
+            const products = response.cart.products.map(product => new Object({ quantity: product.quantity, product: product._id } ));
+            response.cart.products = products;
+
+            res.status(201).send({ success: true, message: 'Orden editada', data: response });
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send({ success: false, message: 'Error al editar pedido', error });
         }
     }
 
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/count/purchased', AuthMiddleware.authorizeUser, OrderController.getPurchasedCount);
 router.get('/groupBy/status', AuthMiddleware.authorizeUser, OrderController.groupByStatus);
 router.post('/', AuthMiddleware.authorizeUser, OrderController.create);
+router.put('/:id', AuthMiddleware.authorizeUser, OrderController.edit);
 router.put('/:id/delivery', AuthMiddleware.authorizeUser, OrderController.assignDelivery);
 
 export default router;
